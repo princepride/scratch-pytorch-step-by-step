49537c3a3bc875acb01d726cc062cd5ef376369e
汪志鹏 <13476859295@163.com>
2024-01-02 17:45:40 +0800

    Update dl.py

--- deep learning/python code/dl.py
+++ deep learning/python code/dl.py
@@ -1,18 +1,23 @@
+import math
 class Tensor:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
 
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
         out._backward = _backward
 
         return out
@@ -37,6 +42,16 @@ class Tensor:
         out._backward = _backward
 
         return out
+    
+    def exp(self):
+        x = self.data
+        out = Tensor(math.exp(x), (self, ), 'exp')
+        
+        def _backward():
+            self.grad += out.data * out.grad
+            out._backward = _backward
+        
+        return out
 
     def relu(self):
         out = Tensor(0 if self.data < 0 else self.data, (self,), 'ReLU')
@@ -46,3 +61,29 @@ class Tensor:
         out._backward = _backward
 
         return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Tensor(t, (self, ), 'tanh')
+        
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+            out._backward = _backward
+        
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
