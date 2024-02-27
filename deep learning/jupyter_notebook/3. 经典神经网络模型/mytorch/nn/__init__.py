9563633a62d16bb0d15e9aa7e6486ae520d975eb
Wang Zhipeng <13476859295@163.com>
2024-02-27 14:44:14 +0800

    rnn forward success, but MSE loss have some error

--- deep learning/jupyter_notebook/3. 经典神经网络模型/mytorch/nn/__init__.py
+++ deep learning/jupyter_notebook/3. 经典神经网络模型/mytorch/nn/__init__.py
@@ -4,3 +4,19 @@ from ..tensor import Tensor
 
 def ReLU(tensor: Tensor) -> Tensor:
     return tensor.relu()
+
+class MSELoss(Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, pred, target):
+        pred = pred.data if isinstance(pred, Tensor) else pred
+        target = target.data if isinstance(target, Tensor) else target
+        def mse_recursive(p, t):
+            if type(p) not in [list, tuple] or type(t) not in [list, tuple]:
+                return (p - t) ** 2
+            else:
+                return sum([mse_recursive(pi, ti) for pi, ti in zip(p, t)]) / len(p)
+
+        total_loss = mse_recursive(pred, target)
+        return Tensor(total_loss)
